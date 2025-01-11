621b8e4a037973cd9f1fbaba08a5dff453743e4c
MayoIsASauce <mja7634@rit.edu>
2025-01-11 18:48:54 -0500

    Final Maintenance and changes

--- CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
+++ CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db5e4f165479d2f7aae0f1f81a1c88e50f7f897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ff7a49d3431a8e82112fd0348530827400cf6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
