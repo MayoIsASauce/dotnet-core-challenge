13fa4e5f0d2d5f05be1b3202455e4dc97898d08a
MayoIsASauce <mja7634@rit.edu>
2025-01-11 16:24:42 -0500

    Maintenance work and updating workspace files

--- CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
+++ CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7fd9eda7d3f36c7daf1394133619e1ee0301d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db5e4f165479d2f7aae0f1f81a1c88e50f7f897")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
